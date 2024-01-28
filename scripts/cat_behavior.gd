f2f84115415a233039240aa005097b1a1e265faf
Razzi Abuissa <razzi@abuissa.net>
2024-01-27 18:32:59 -0600

    Cat stands up

--- scripts/cat_behavior.gd
+++ scripts/cat_behavior.gd
@@ -34,11 +34,22 @@ func _ready():
 	target = get_parent().get_node("Block")
 
 func _process(delta):
+	var stopped = get_linear_velocity().length() < .0001
+	if stopped:
+		print('stopped')
+		global_rotation = Vector3(0, 0, 0)
+
 	if current_state == CurrentState.PLAY:
 		play()
+	elif current_state == CurrentState.STAY:
+		stay()
 
 func play():
 	var directionToTarget = target.global_position - global_position
 	print("directionToTarget: " + str(directionToTarget))
-	apply_impulse(directionToTarget)
+	apply_impulse(directionToTarget * 3)
 	current_state = CurrentState.STAY
+
+func stay():
+	"""Makes the cat stand up."""
+	print("standing up")
