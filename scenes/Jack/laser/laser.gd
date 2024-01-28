aeef69fbcdae53ce449704009f75bb6845237ff6
Jack Stubblefield <jack011601@gmail.com>
2024-01-27 18:55:33 -0600

    going outside for enrichment time

--- scenes/Jack/laser/laser.gd
+++ scenes/Jack/laser/laser.gd
@@ -1,12 +1,13 @@
-extends Node3D
-@onready var laserPosition = get_node("../")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	get_node(".").visible = true
-	print(get_tree())	#print(character.get_child_count())
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#extends Node3D
+#@onready var laserPosition
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#$Character.laserPosition
+	#print(str(laserPosition))
+#
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
